fc90fd163b824b74a1c01caa564723a4ce6f1138
Iswar <sahuiswa007@outlook.com>
2023-09-14 21:13:16 +0530

    new file:   src/components/Weatherapp/WeatherApp.css 	new file:   src/components/images/clear.png 	new file:   src/components/images/cloud.png 	new file:   src/components/images/drizzle.png 	new file:   src/components/images/humidity.png 	new file:   src/components/images/rain.png 	new file:   src/components/images/search.png 	new file:   src/components/images/snow.png 	new file:   src/components/images/wind.png

--- src/components/Weatherapp/WeatherApp.jsx
+++ src/components/Weatherapp/WeatherApp.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './WeatherApp.css';
 import search_icon from '../images/search.png';
 import clear_icon from '../images/clear.png';
@@ -9,13 +9,62 @@ import rain_icon from '../images/rain.png';
 import snow_icon from '../images/snow.png';
 import wind_icon from '../images/wind.png';
 
-const WeatherApp = () => {
+const WeatherApp =  () => {
   let api_key = "2f9063d606d1add1eb33c080965bcdfa";
-  const search = () =>{
+  const [wicon,setWicon] = useState(cloud_icon);
+  const search = async () =>{
     const element = document.getElementsByClassName("cityinput");
     if (element[0].value === ""){
       return 0;
     }
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${api_key}`;
+    let response = await fetch(url);
+    let data = await response.json();
+    const humidity = document.getElementsByClassName("humidity-percent");
+    const wind = document.getElementsByClassName("wind-rate");
+    const temprature = document.getElementsByClassName("weather-temp");
+    const location = document.getElementsByClassName("weather-location");
+
+    humidity[0].innerHTML = data.main.humidity+"%";
+    wind[0].innerHTML = Math.floor(data.wind.speed)+"km/h";
+    temprature[0].innerHTML = Math.floor(data.main.temp)+"°c";
+    location[0].innerHTML = data.name;
+
+    if (data.weather[0].icon === "01d" || data.weather[0].icon === "01n")
+    {
+      setWicon(clear_icon);
+    }
+    else if(data.weather[0].icon === "02d" || data.weather[0].icon === "02n")
+    {
+      setWicon(cloud_icon);
+    }
+    else if(data.weather[0].icon === "03d" || data.weather[0].icon === "03n")
+    {
+      setWicon(drizzle_icon);
+    }
+    else if (data.weather[0].icon === "04d" || data.weather[0].icon === "04n")
+    {
+      setWicon(drizzle_icon);
+    }
+    else if(data.weather[0].icon === "09d" || data.weather[0].icon === "09n")
+    {
+      setWicon(rain_icon);
+    }
+    else if(data.weather[0].icon === "10d" || data.weather[0].icon === "10n")
+    {
+      setWicon(rain_icon);
+    }
+    else if(data.weather[0].icon === "13d" || data.weather[0].icon === "13n")
+    {
+      setWicon(snow_icon);
+    }
+    else
+    {
+      setWicon(clear_icon);
+    }
+
+
+
     
   }
   return (
@@ -23,12 +72,13 @@ const WeatherApp = () => {
       <div className="container">
         <div className="top-bar">
           <input type="text" className='cityinput' placeholder='search'/>
-          <div className="search-icon">
+          <div className="search-icon" onClick={search}>
             <img src={search_icon} alt="search" />
           </div>
         </div>
         <div className="weather-image">
-          <img src={cloud_icon} alt="cloud" />
+          <img src={wicon} alt="" /> 
+          
         </div>
         <div className="weather-temp">24°c</div>
         <div className="weather-location">London</div>
@@ -43,7 +93,7 @@ const WeatherApp = () => {
           <div className="element">
             <img src={wind_icon} alt="" className="icon" />
             <div className="data">
-              <div className="humidity-percent">18km/hr</div>
+              <div className="wind-rate">18km/hr</div>
               <div className="text">Wind Speed</div>
             </div>
           </div>
